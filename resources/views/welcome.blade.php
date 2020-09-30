a460ffdad1447fe3c0c7375827f14b99645eb139
juanbpadilla <juanbpadillac@gmail.com>
2020-09-30 05:29:17 -0400

    welcome change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,7 @@
                         </g>
                     </svg>
                 </div>
+                <h2>TECNOLOGIA DE PROGRAMACION EN RED</2>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
